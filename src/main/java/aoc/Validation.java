c282da3cc50661d220e8194e02d821b949dc1e49
Moonlight <moonlight9512@gmail.com>
2025-12-09 22:12:36 +0500

    Add console output utility

--- src/main/java/aoc/Validation.java
+++ src/main/java/aoc/Validation.java
@@ -5,6 +5,11 @@ package aoc;
  * Handy validation for the Main classes.
  */
 public class Validation {
+    
+    public static void print(String title, Object printedResult) {
+        System.out.printf("[%s]%n", title);
+        System.out.printf("The answer is: %s%n", printedResult.toString());
+    }
 
     public static <T> void validate(T expected, T result) {
         if (expected.equals(result)) {
