1f980c96f8602d7796e25a13302696e0b21fa85f
Moonlight <moonlight9512@gmail.com>
2025-12-11 22:13:43 +0500

    Update Rect class structure

--- src/main/java/aoc/p009/Rect.java
+++ src/main/java/aoc/p009/Rect.java
@@ -1,5 +1,7 @@
 package aoc.p009;
 
+import java.util.Objects;
+
 import aoc.shared.LongVector2;
 
 public class Rect {
@@ -8,6 +10,9 @@ public class Rect {
     public final LongVector2 b;
     public final LongVector2 c;
     public final LongVector2 d;
+    
+    private LongVector2 center = null;
+    private long area = 0;
     
     
     public Rect(LongVector2 start, LongVector2 opposite) {
@@ -24,6 +29,16 @@ public class Rect {
         this.c = c;
         
         d = new LongVector2(a.x, c.y);
+    }
+    
+    
+    public boolean overlaps(Rect another) {
+        boolean left = x2() < another.x1();
+        boolean right = x1() > another.x2();
+        boolean top = y2() < another.y1();
+        boolean bottom = y1() > another.y2();
+        
+        return !(left || right || top || bottom);
     }
     
     
@@ -44,7 +59,40 @@ public class Rect {
     }
     
     
+    public LongVector2 center() {
+        return center != null ? center : generateCenter();
+    }
+    
+    
     public long square() {
-        return (x2() - x1() + 1) * (y2() - y1() + 1);
+        return area != 0 ? area : generateArea();
+    }
+    
+    
+    private long generateArea() {
+        area = (x2() - x1() + 1) * (y2() - y1() + 1);
+        return area;
+    }
+    
+    private LongVector2 generateCenter() {
+        center = new LongVector2(
+                x1() + (x2() - x1()) / 2, 
+                y1() + (y2() - y1()) / 2
+                );
+        return center;
+    }
+    
+    
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Rect r) {
+            return hashCode() == r.hashCode();
+        }
+        return false;
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b, c, d);
     }
 }
