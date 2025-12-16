e7f67b7d6400c5ab2b3e4de8d449a55613cb503f
Moonlight <moonlight9512@gmail.com>
2025-12-16 23:14:48 +0500

    Partially fix division problem for Matrix

--- src/main/java/aoc/p010/solvers/Matrix.java
+++ src/main/java/aoc/p010/solvers/Matrix.java
@@ -9,7 +9,7 @@ import aoc.p010.factory.StartedMachine;
 public class Matrix {
     
     /** Prints the matrix to the console when it changes. */
-    public final boolean debug = true;
+    public final boolean debug = false;
     
     /** Number of columns. */
     public final int m;
@@ -101,16 +101,23 @@ public class Matrix {
         // In case there are more rows than columns
         int end = Math.min(n, m);
         
-        for (int i = 0; i < end; i++) {
-            // Set the first element of the first row to != 0 (swap)
-            // Skip if not possible
-            if (!swapRows(i)) {
-                continue;
+        for (int r = 0; r < end; r++) {
+            int c = r;
+            
+            // Set the first element of the leading column to != 0 (swap)
+            // Skip to the next row if not possible
+            while (!swapRows(c)) {
+                c++;
+            }
+            
+            // Out of bounds; can't proceed further
+            if (c == m) {
+                break;
             }
             
             // Make all other rows have 0 at the i column
-            for (int j = i + 1; j < n; j++) {
-                subtract(table[j], table[i], i);
+            for (int j = r + 1; j < n; j++) {
+                subtract(table[j], table[r], c);
             }
             
             print();
@@ -172,7 +179,7 @@ public class Matrix {
      * conditions and must be skipped.
      */
     private boolean checkRREF(int row, int column) {
-        // Out of bounds: skip
+        // Out of bounds: break
         if (column == m) {
             return true;
         }
@@ -185,23 +192,24 @@ public class Matrix {
      * Checks all the rows in the table and makes it so the table
      * has a non-zero element at position [start][start] by swapping rows
      * (if it is necessary and possible).
-     * @param start The position of the first leading element 
+     * @param leadingColumn The position of the first leading element 
      * in the table during the current iteration of REF.
      * @return True if the rows can be applied REF transformation
      * during the current iteration. If it's false, must skip 
-     * to the next iteration.
+     * to the next column. Also returns true if reached the last
+     * column; then the loop must be broken.
      */
-    private boolean swapRows(int start) {
+    private boolean swapRows(int leadingColumn) {
         // Reached the limit (by columns), cannot swap
-        if (start >= m) {
-            return false;
+        if (leadingColumn >= m) {
+            return true;
         }
         
         boolean isSwappable = false;
         
-        for (int i = start; i < n; i++) {
+        for (int i = leadingColumn; i < n; i++) {
             // Not zero, seek further
-            if (table[i][start] == 0) {
+            if (table[i][leadingColumn] == 0) {
                 continue;
             }
             
@@ -209,13 +217,13 @@ public class Matrix {
             isSwappable = true;
             
             // Need not to swap
-            if (i == start) {
+            if (i == leadingColumn) {
                 break;
             }
             
             // Swap
-            int[] temp = table[start];
-            table[start] = table[i];
+            int[] temp = table[leadingColumn];
+            table[leadingColumn] = table[i];
             table[i] = temp;
             break;
         }
@@ -239,26 +247,75 @@ public class Matrix {
             int leadingColumn
             ) {
         
-        // The division should not have a remainder
-        if (minuend[leadingColumn] % subtrahend[leadingColumn] != 0) {
-            String message = "Not dealing with Integers (subtraction error)";
-            String details = String.format(
-                    "%d / %d at column %d", 
-                    minuend[leadingColumn], 
-                    subtrahend[leadingColumn],
-                    leadingColumn
-                            );
-            throw matrixIncompatible(
-                    String.format("%s: %s", message, details)
-                    );
-        }
-        
         int len = minuend.length;
-        int ratio = minuend[leadingColumn] / subtrahend[leadingColumn];
+        int minuendLead = minuend[leadingColumn];
+        int subtrahendLead = subtrahend[leadingColumn];
         
         for (int i = leadingColumn; i < len; i++) {
-            minuend[i] -= ratio * subtrahend[i];
+            minuend[i] = subtract(
+                    minuend[i], 
+                    subtrahend[i], 
+                    minuendLead,
+                    subtrahendLead
+                    );
         }
+    }
+    
+    private int subtract(
+            int minuend, 
+            int subtrahend, 
+            int minuendLead, 
+            int subtrahendLead
+            ) {
+        
+        int ratio = 0;
+        int result = 0;
+        
+        if ( isLeftGreaterByAbs(minuendLead, subtrahendLead) ) {
+            ratio = minuendLead / subtrahendLead;
+            result = minuend - subtrahend * ratio;
+            
+        } else {
+            ratio = subtrahendLead / minuendLead;
+            result = minuend * ratio - subtrahend;
+            
+        }
+        
+        return result;
+    }
+    
+    /**
+     * Checks if the given two number can be divided in any way. <br>
+     * If the absolute value of numA is greater than the absolute value
+     * of numB, tries to divide {@code numA / numB} without a remainder.
+     * <br> Otherwise, tries to divide {@code numB / numA}. <br>
+     * Throws runtime exception if no division can be performed.
+     * @param numA One of the candidates for division.
+     * @param numB The other candidate for division.
+     * @return True if {@code abs(numA) > abs(numB)} 
+     * and {@code numB != 0} (so numA should be divided by numB);
+     * false otherwise.
+     */
+    private boolean isLeftGreaterByAbs(int numA, int numB) {
+        if (numA == 0) {
+            return true;
+        }
+        
+        if (numB == 0) {
+            return false;
+        }
+        
+        int absA = Math.abs(numA);
+        int absB = Math.abs(numB);
+        
+        int min = Math.min(absA, absB);
+        int max = Math.max(absA, absB);
+        
+        if (max % min != 0) {
+            throw divisionException(numA, numB);
+        }
+        
+        return absA > absB;
     }
     
     /**
@@ -291,6 +348,24 @@ public class Matrix {
     private IllegalStateException matrixIncompatible(String message) {
         return new IllegalStateException(
                 String.format("The matrix is incompatible: %s.", message)
+                );
+    }
+    
+    /**
+     * Returns a template for division exception.
+     * @return
+     */
+    private IllegalStateException divisionException(
+            int dividend, 
+            int divisor
+            ) {
+        
+        return new IllegalStateException(
+                String.format(
+                        "Floating point division: %d / %d.", 
+                        dividend, 
+                        divisor
+                        )
                 );
     }
 }
