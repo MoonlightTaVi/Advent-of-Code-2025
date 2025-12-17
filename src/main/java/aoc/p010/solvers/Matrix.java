2335bff89e30124ac21199934c180d13924dc801
Moonlight <moonlight9512@gmail.com>
2025-12-17 20:05:24 +0500

    Fix division remainder problem for Matrix

--- src/main/java/aoc/p010/solvers/Matrix.java
+++ src/main/java/aoc/p010/solvers/Matrix.java
@@ -84,7 +84,7 @@ public class Matrix {
      * @return 2D integer array of the RREF of the matrix.
      */
     public int[][] build() {
-        matrixToREF();
+        //matrixToREF();
         matrixToRREF();
         
         return table;
@@ -93,279 +93,197 @@ public class Matrix {
     
     /**
      * Transforms this matrix into the row echelon form. <br>
-     * For testing purposes only, use build() instead.
+     * For testing purposes only, use {@code build()} instead.
      */
+    @Deprecated
     public void matrixToREF() {
-        print();
+        int row = 0;
+        int column = 0;
         
-        // In case there are more rows than columns
-        int end = Math.min(n, m);
-        
-        for (int r = 0; r < end; r++) {
-            int c = r;
+        while (row < n && column < m) {
             
-            // Set the first element of the leading column to != 0 (swap)
-            // Skip to the next row if not possible
-            while (!swapRows(c)) {
-                c++;
+            // Find a row that has an appropriate leading element
+            int pivotRow = findPivotRow(row, column);
+            
+            // Not possible to form a pivot
+            if (pivotRow == -1) {
+                // TODO Increment free variables count
+                // Skip to the next column
+                column++;
+                continue;
             }
             
-            // Out of bounds; can't proceed further
-            if (c == m) {
-                break;
+            // Swap current row and pivot row
+            swapRows(row, pivotRow);
+            
+            // Make current (pivot) row have 1 as its leading element
+            //divideByLeading(table[row], column);
+            
+            // Make all rows below the current row (ONLY below)
+            //  have zeroes at the pivot column
+            //  (so the only non-zero element of this column
+            //   BELOW the pivot row and including the pivot row
+            //   is the leading element (1) of the pivot row)
+            //    (techically, it's not a pivot row this way)
+            for (int i = row + 1; i < n; i++) {
+                subtract(table[i], table[row], column);
             }
             
-            // Make all other rows have 0 at the i column
-            for (int j = r + 1; j < n; j++) {
-                subtract(table[j], table[r], c);
-            }
-            
-            print();
+            // Next step
+            row++;
+            column++;
         }
     }
     
     /**
-     * Transforms this matrix into the reduced raw echelon form. <br>
+     * Transforms this matrix into the reduced row echelon form. <br>
      * For testing purposes only, use build() instead.
      */
     public void matrixToRREF() {
-        for (int row = n - 1; row > 0; row--) {
-            int column = findLeadingColumn(row);
-            
-            // Check if the row is already at final form
-            if (checkRREF(row, column)) {
-                continue;
-            }
-
-            int lead = table[row][column];
-            // Set the lead row to 1 by changing all of the row values
-            // Division by 1 means nothing, so just skip it
-            if (lead != 1) {
-                divide(table[row], column);
-            }
-            
-            // Set the element above this leading element to 0
-            // Change the other elements in the row above respectively
-            subtract(table[row - 1], table[row], column);
-            
-            print();
-        }
-    }
-    
-    
-    /**
-     * Finds the first non-zero element in a row and returns the index
-     * of the corresponding column.
-     * @param row
-     * @return The ID of the leading column of the row.
-     */
-    private int findLeadingColumn(int row) {
-        // Find the leading (pivot) column
+        int row = 0;
         int column = 0;
         
-        while (column < m && table[row][column] == 0) {
-            column++;
-        }
-        
-        // Returns an out-of-bounds index if there're only zeroes
-        return column;
-    }
-    
-    /**
-     * Checks if this row can (and should) be applied a row reduction.
-     * @param row Row to test against conditions of RREF.
-     * @param column Leading column of this row.
-     * @return True if this row already corresponds to the RREF
-     * conditions and must be skipped.
-     */
-    private boolean checkRREF(int row, int column) {
-        // Out of bounds: break
-        if (column == m) {
-            return true;
-        }
-        
-        // Return true if it already corresponds to RREF (skip it)
-        return table[row][column] == 1 && table[row - 1][column] == 0;
-    }
-    
-    /**
-     * Checks all the rows in the table and makes it so the table
-     * has a non-zero element at position [start][start] by swapping rows
-     * (if it is necessary and possible).
-     * @param leadingColumn The position of the first leading element 
-     * in the table during the current iteration of REF.
-     * @return True if the rows can be applied REF transformation
-     * during the current iteration. If it's false, must skip 
-     * to the next column. Also returns true if reached the last
-     * column; then the loop must be broken.
-     */
-    private boolean swapRows(int leadingColumn) {
-        // Reached the limit (by columns), cannot swap
-        if (leadingColumn >= m) {
-            return true;
-        }
-        
-        boolean isSwappable = false;
-        
-        for (int i = leadingColumn; i < n; i++) {
-            // Not zero, seek further
-            if (table[i][leadingColumn] == 0) {
+        while (row < n && column < m) {
+            
+            // Find a row that has an appropriate leading element
+            int pivotRow = findPivotRow(row, column);
+            
+            // Not possible to form a pivot
+            if (pivotRow == -1) {
+                // TODO Increment free variables count
+                // Skip to the next column
+                column++;
                 continue;
             }
             
-            // Success
-            isSwappable = true;
+            // Swap current row and pivot row
+            swapRows(row, pivotRow);
             
-            // Need not to swap
-            if (i == leadingColumn) {
-                break;
+            // Make current (pivot) row have 1 as its leading element
+            divideByLeading(table[row], column);
+            
+            // Make all rows above and below the current row
+            //  have zeroes at the pivot column
+            //  (so the only non-zero element of this column
+            //   is the leading element (1) of the pivot row)
+            for (int i = 0; i < n; i++) {
+                if (i == row) {
+                    continue;
+                }
+                
+                subtract(table[i], table[row], column);
             }
             
-            // Swap
-            int[] temp = table[leadingColumn];
-            table[leadingColumn] = table[i];
-            table[i] = temp;
-            break;
+            // Next step
+            row++;
+            column++;
+        }
+    }
+    
+    
+    /**
+     * Makes two rows in the matrix swap their position.
+     * @param firstId The ID of the first row in the matrix.
+     * @param secondId The ID of the second row in the matrix.
+     */
+    private void swapRows(int firstId, int secondId) {
+        // The same row
+        if (firstId == secondId) {
+            return;
         }
         
-        return isSwappable;
+        int[] temp = table[firstId];
+        table[firstId] = table[secondId];
+        table[secondId] = temp;
     }
     
     /**
-     * Subtracts one row from another. As the result, the first row
-     * will have 0 at its {@code leadingColumn} position (i.e. above
-     * the leading element of the second row). <br>
-     * Part of the RREF transformation algorithm.
-     * @param minuend Row to subtract from.
-     * @param subtrahend Row to subtract from the minuend.
-     * @param leadingColumn The leading element position in the
-     * subtrahend row.
+     * Finds the ID of the row in the matrix that corresponds
+     * to the appropriate conditions of a pivot row.
+     * @param row Row to start searching from.
+     * @param column Pivot column that must have the leading element.
+     * @return The ID of the first row that can form a pivot.
+     * Returns -1 if this column cannot form a pivot and must be skipped.
      */
-    private void subtract(
-            int[] minuend, 
-            int[] subtrahend, 
-            int leadingColumn
-            ) {
+    private int findPivotRow(int row, int column) {
+        int result = -1;
         
-        int len = minuend.length;
-        int minuendLead = minuend[leadingColumn];
-        int subtrahendLead = subtrahend[leadingColumn];
-        
-        for (int i = leadingColumn; i < len; i++) {
-            minuend[i] = subtract(
-                    minuend[i], 
-                    subtrahend[i], 
-                    minuendLead,
-                    subtrahendLead
-                    );
-        }
-    }
-    
-    private int subtract(
-            int minuend, 
-            int subtrahend, 
-            int minuendLead, 
-            int subtrahendLead
-            ) {
-        
-        int ratio = 0;
-        int result = 0;
-        
-        if ( isLeftGreaterByAbs(minuendLead, subtrahendLead) ) {
-            ratio = minuendLead / subtrahendLead;
-            result = minuend - subtrahend * ratio;
-            
-        } else {
-            ratio = subtrahendLead / minuendLead;
-            result = minuend * ratio - subtrahend;
-            
+        for (int i = row; i < n; i++) {
+            if (checkDivision(table[i], column)) {
+                result = i;
+                break;
+            }
         }
         
         return result;
     }
     
     /**
-     * Checks if the given two number can be divided in any way. <br>
-     * If the absolute value of numA is greater than the absolute value
-     * of numB, tries to divide {@code numA / numB} without a remainder.
-     * <br> Otherwise, tries to divide {@code numB / numA}. <br>
-     * Throws runtime exception if no division can be performed.
-     * @param numA One of the candidates for division.
-     * @param numB The other candidate for division.
-     * @return True if {@code abs(numA) > abs(numB)} 
-     * and {@code numB != 0} (so numA should be divided by numB);
-     * false otherwise.
+     * Checks if each element of this row can be divided by the leading
+     * element of this row without a remainder (this is necessary to form
+     * a pivot).
+     * @param row Row to be tested against the appropriate conditions
+     * of forming a pivot.
+     * @param leadID Position of the leading element in the row.
+     * @return True if this row can form a pivot column, false otherwise.
      */
-    private boolean isLeftGreaterByAbs(int numA, int numB) {
-        if (numA == 0) {
-            return true;
-        }
+    private boolean checkDivision(int[] row, int leadID) {
+        int lead = row[leadID];
         
-        if (numB == 0) {
+        if (lead == 0) {
             return false;
         }
         
-        int absA = Math.abs(numA);
-        int absB = Math.abs(numB);
+        boolean success = true;
         
-        int min = Math.min(absA, absB);
-        int max = Math.max(absA, absB);
-        
-        if (max % min != 0) {
-            throw divisionException(numA, numB);
-        }
-        
-        return absA > absB;
-    }
-    
-    /**
-     * Divides every element of this row by the leading element
-     * of this row. <br>
-     * As the result, the leading element becomes 1.
-     * @param row
-     * @param leadingColumn
-     */
-    private void divide(int[] row, int leadingColumn) {
-        int lead = row[leadingColumn];
-        for (int j = leadingColumn; j < m; j++) {
-            
-            if (row[j] % lead != 0) {
-                throw matrixIncompatible(
-                        "Not dealing with integers (division error)"
-                        );
+        for (int i = leadID + 1; i < m; i++) {
+            if (row[leadID] % lead != 0) {
+                success = false;
+                break;
             }
-            
-            row[j] /= lead;
+        }
+        
+        return success;
+    }
+    
+    
+    /**
+     * Divides each element in this row (starting from {@code leadID}
+     * column) by its leading element (positioned at {@code leadID}
+     * column). As the result, the leading element will become 1.
+     * @param row Row to divide the elements of.
+     * @param leadId Position of the leading element in the row.
+     */
+    private void divideByLeading(int[] row, int leadId) {
+        int lead = row[leadId];
+        
+        for (int i = leadId; i < m; i++) {
+            row[i] /= lead;
         }
     }
     
-    
     /**
-     * Returns a template for matrix exception.
-     * @param message Specific message about the exception.
-     * @return
+     * Subtracts {@code thisRow} from {@code fromRow}, which means
+     * subtracts each element of the subtrahend row (starting from
+     * {@code coefId} ) from the element of the minuend row that is
+     * positioned at the same column. The elements of the subtrahend
+     * row are multiplied by a coefficient before subtraction;
+     * the coefficient equals {@code fromRow[coefId]}. <br>
+     * As the result, {@code fromRow[coefId]} will be equal to 0
+     * (given that {@code thisRow[coefId]} equals 1).
+     * @param fromRow Row to subtract from (minuend).
+     * @param thisRow Row to subtract from the previous row (subtrahend).
+     * @param coefId Position of the coefficient in the minuend row.
+     * Each element of the subtrahend row will be multiplied by
+     * this coefficient before subtraction.
      */
-    private IllegalStateException matrixIncompatible(String message) {
-        return new IllegalStateException(
-                String.format("The matrix is incompatible: %s.", message)
-                );
-    }
-    
-    /**
-     * Returns a template for division exception.
-     * @return
-     */
-    private IllegalStateException divisionException(
-            int dividend, 
-            int divisor
-            ) {
+    private void subtract(int[] fromRow, int[] thisRow, int coefId) {
+        // thisRow[coefId] must be equal to 1
+        //  But double check for bugs
+        int coef = fromRow[coefId] / thisRow[coefId];
         
-        return new IllegalStateException(
-                String.format(
-                        "Floating point division: %d / %d.", 
-                        dividend, 
-                        divisor
-                        )
-                );
+        for (int i = coefId; i < m; i++) {
+            fromRow[i] -= thisRow[i] * coef;
+        }
     }
 }
